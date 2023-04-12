694b46c5a04ab8b1dfce5ad0df63fbf889f3d68d
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-13 02:20:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+// complete this js code
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+Person.prototype.greet=function(){
+	return "Hello, my name is " + this.name +", I am "+this.age+" years old.";
+};
+// var p=new Person('areeb',23);
+// console.log(p);
+// console.log(Object.getPrototypeOf(p));
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
+	this.jobTitle=jobTitle;
+}
+
+Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.jobGreet=function(){
+	return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+};
 
 // Do not change code below this line
 window.Person = Person;
